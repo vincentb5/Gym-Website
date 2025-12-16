547abbcd3bd5c527f10e89a1ab63a2d4b146af70
vincentb5 <vincent.bastien@outlook.com>
2025-12-16 11:37:33 -0800

    In Class Update (BROKEN)

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__, static_folder='static', static_url_path='')
+app = Flask(__name__, static_folder='static')
 
 
 @app.route('/')
 def home():
     # Serve the static `index.html` from the `static/` folder
-    return app.send_static_file('index.html')
+    return render_template('index.html')
 
 
 @app.route('/health')
@@ -14,6 +14,28 @@ def health():
     # Lightweight health endpoint for checks
     return 'Server is running!'
 
+@app.route("/step1")
+def step1():
+    return render_template("step1.html")
+
+@app.route("/step2")
+def step2():
+    return render_template("step2.html")
+
+@app.route("/step3")
+def step3():
+    return render_template("step3.html")
+
+@app.route("/step4")
+def step4():
+    return render_template("step4.html")
+
+@app.route("/step5")
+def step5():
+    return render_template("step5.html")
+
+
+
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    app.run(debug=True, port=8000, host='0.0.0.0')
